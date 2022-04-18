a16c8bc3f143aa9843fb764989dd0070efcfe1dd
osmanidik <osmanidikresmi@gmail.com>
2022-04-18 15:34:49 +0000

    style parity.

--- src/App.js
+++ src/App.js
@@ -30,12 +30,12 @@ const uiConfig = {
 };
 
 const registerStudent = (name, surname, number) => {
-  db.collection('students').add({
+  db.collection('students').doc(number).set({
     uid: firebase.auth().currentUser.uid,
     name: name,
     surname: surname,
     number: number
-  });
+  }, { merge: true });
 };
 
 const exportUser = (user) => {
@@ -74,26 +74,33 @@ function App() {
 
   if (!isSignedIn) {
     return (
-      <div>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      <div className='App'>
+        <div className='login' style={{backgroundColor: "#fffafa"}}>
+          <div className="card buttonLogin">
+              <h3 style={{color: "black"}}>Welcome!</h3>
+              <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+            </div>
+        </div>
       </div>
     );
   }
 
   if (!isRegistered) {
     return (
-      <div className='register'>
-        <h1 className='regtitle'>Sistemde kayıtlı değilsiniz. Lütfen bilgilerinizi giriniz.</h1>
-        <span className='respan'>Adınız:</span>
-        <input className='reginput' onChange={(e) => setName(e.target.value)}/>
-        <span className='respan'>Soyadınız:</span>
-        <input className='reginput' onChange={(e) => setSurname(e.target.value)}/>
-        <span className='respan'>Numaranız:</span>
-        <input className='reginput' onChange={(e) => setNumber(e.target.value)}/>
-        <br/>
-        <button className='regbutton' onClick={() => registerStudent(name, surname, number)}>Kaydol</button>
-        <br/>
-        <button className='regexit' onClick={() => {firebase.auth().signOut();localStorage.clear();setIsSignedIn(false);setIsRegistered(false);}}>Çıkış Yap</button>
+      <div className='App'>
+        <div className='register'>
+          <h3 className='regtitle'>Sistemde kayıtlı değilsiniz. Lütfen bilgilerinizi giriniz.</h3>
+          <h5 className='regspan'>Adınız:</h5>
+          <input className='reginput' onChange={(e) => setName(e.target.value)}/>
+          <h5 className='regspan'>Soyadınız:</h5>
+          <input className='reginput' onChange={(e) => setSurname(e.target.value)}/>
+          <h5 className='regspan'>Numaranız:</h5>
+          <input className='reginput' onChange={(e) => setNumber(e.target.value)}/>
+          <br/>
+          <button className='regbutton' onClick={() => registerStudent(name, surname, number)}>Kaydol</button>
+          <br/>
+          <button className='regbutton' onClick={() => {firebase.auth().signOut();localStorage.clear();setIsSignedIn(false);setIsRegistered(false);}}>Çıkış Yap</button>
+        </div>
       </div>
     );
   }
@@ -101,9 +108,13 @@ function App() {
   exportUser(firebase.auth().currentUser);
 
   return (
-    <div>
-      <h1>Exam Guard</h1>
-      <p>Merhaba {firebase.auth().currentUser.email}! Giriş yaptınız! Bu sayfayı kapatarak Exam Guard programından devam edebilirsiniz.</p>
+    <div className='App'>
+      <div className='login' style={{backgroundColor: "#fffafa"}}>
+        <div className="card success">
+          <h3>Exam Guard</h3>
+          <span>Merhaba {firebase.auth().currentUser.displayName}! Giriş yaptınız! Bu sayfayı kapatarak Exam Guard programından devam edebilirsiniz.</span>
+        </div>
+      </div>
     </div>
   );
 }
